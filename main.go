d07494398632a82edd1cb73bf429fca9e79e8537
seamount-knight <15010828953@163.com>
2019-04-30 18:23:00 +0800

    upgrade to v2

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/seamounts/go-mod-tidy/cmd"
+	"os"
 )
 
 func main() {
-	cmd.Execute()
+	if err := cmd.GoMod.Execute(); err != nil {
+		fmt.Println(err)
+
+		os.Exit(1)
+	}
 }
